f99864d4d22ffab0d6e51a7a5071444274058389
mahboubchari <mahboubchari@gmail.com>
2025-04-22 00:50:12 +0330

    azafeh kardan class EightQueensGA va tabe  __init__ to Scode.py

--- Scode.py
+++ Scode.py
@@ -2,8 +2,4 @@ import random
 
 class EightQueensGA:
     def __init__(self, population_size=100, mutation_rate=0.1, generations=500):
-        self.n = 8  # تعداد وزیرها
-        self.population_size = population_size  # (تعدادکروموزوم)اندازه جمعیت
-        self.mutation_rate = mutation_rate  # نرخ جهش
-        self.generations = generations  # تعداد نسل‌هایی که باید اجراشود
-        self.population = self.initialize_population()  # جمعیت اولیه
+        
