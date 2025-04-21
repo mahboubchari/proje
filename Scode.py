de3732ebc7aae2654325aa3357f0db256ace263e
mahboubchari <mahboubchari@gmail.com>
2025-04-22 01:48:18 +0330

    changed Scode.py in changing branch . i add def random_solution

--- Scode.py
+++ Scode.py
@@ -10,4 +10,9 @@ class EightQueensGA:
         
     def initialize_population(self):
         """ایجاد جمعیت اولیه به صورت تصادفی."""
-        return [self.random_solution() for _ in range(self.population_size)]    
+        return [self.random_solution() for _ in range(self.population_size)]
+
+    
+    def random_solution(self):    #لیستی از موقعیت وزیرها    
+        """ایجاد یک راه‌حل تصادفی."""
+        return random.sample(range(self.n), self.n) 
