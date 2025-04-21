81e5a07b7fe59818e26408b0055b73f794ec47ed
mahboubchari <mahboubchari@gmail.com>
2025-04-22 01:10:51 +0330

    azafeh kardan tabe initialize_populatio  va ijad jamet tasadofi to Scode.py

--- Scode.py
+++ Scode.py
@@ -8,3 +8,6 @@ class EightQueensGA:
         self.generations = generations  # تعداد نسل‌هایی که باید اجراشود
         self.population = self.initialize_population()  # جمعیت اولیه
         
+    def initialize_population(self):
+        """ایجاد جمعیت اولیه به صورت تصادفی."""
+        return [self.random_solution() for _ in range(self.population_size)]    
